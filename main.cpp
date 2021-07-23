f6ae5feebf375ec2724c4a039b22f19ad65c1ed0
BlaiseGD <58518531+BlaiseGD@users.noreply.github.com>
2021-07-22 20:30:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,59 +1,115 @@
 #include <iostream>
 #include "months.h"
 
-/*Jan 31 days
-Feb 28 days
-March 31 days
-April 30 days
-May 31 days
-June 30 days
-July 31 days
-August 31 days
-September 30 days
-October 31 days 
-Novemver 30 days 
-December 31 days*/
-
 int main(){
-    int year; //choice of year for calendar
-    int leapyearFeb = (year % 4 > 0) ? 28 : 29; //ternary operator to decide for leap year
     int menuChoice; //allows choice of 1 or 2
-    int feb = 2; //what month february is seperate due to leap year
     int monthChoice;
-
-    typedef struct thirty{  //this is the order of months for 30 day months
-        int april = 4;
-        int june = 6;
-        int september = 9;
-        int november = 11;
-    }M30;
-
-    typedef struct thirtyone{   //this is the order of months for 30 day months
-        int jan = 1;
-        int march = 3;
-        int may = 5;
-        int july = 7;
-        int august = 8;
-        int october = 10;
-        int december = 12;
-    }M31;
 
     std::cout << "Select an option:" << std::endl;
     std::cout << "1. Make your own calendar" << std::endl;
     std::cout << "2. View calendar (default)" << std::endl;
     std::cin >> menuChoice;
     system("clear");
+    monthMistake:
     if(menuChoice == 1){
         std::cin.clear();
         customCalendar();
+        std::cout << "Would you like to save your calendar?" << std::endl;
+        //add in a save system for calendar
+        exit(0);
     }
     else{    
         std::cout << "Year for calendar?" << std::endl;
         std::cin >> year;
         std::cout << "What month do you wanna display?" << std::endl;
+        std::cin >> monthChoice;
     }
 
-    //Make a switch statement for choice of month
+    int leapyearFeb = (year % 4 > 0) ? 28 : 29; //ternary operator to decide for leap year
+
+
+    switch(monthChoice){
+        case 1: 
+        system("clear");
+        std::cout << "     Januaray" << std::endl;
+        thirtyOneMonth();
+        std::cin.clear();
+        break;
+        case 2: 
+        system("clear");
+        std::cout << "     February" << std::endl;
+        std::cin.clear();
+        for(int i = 1; i <= leapyearFeb; i++){
+            printf("|%d", i);
+            if(i % 7 == 0) std::cout << std::endl;
+        }
+        break;
+        case 3:
+        system("clear");
+        std::cout << "     March" << std::endl;
+        thirtyOneMonth();
+        std::cin.clear();
+        break;
+        case 4:
+        system("clear");
+        std::cout << "     April" << std::endl;
+        thirtyMonth();
+        std::cin.clear();
+        break;
+        case 5:
+        system("clear");
+        std::cout << "    May" << std::endl;
+        thirtyOneMonth();
+        std::cin.clear();
+        break;
+        case 6:
+        system("clear");
+        std::cout << "     June" << std::endl;
+        thirtyMonth();
+        std::cin.clear();
+        break;
+        case 7:
+        system("clear");
+        std::cout << "     July" << std::endl;
+        thirtyOneMonth();
+        std::cin.clear();
+        break;
+        case 8:
+        system("clear");
+        std::cout << "     August" << std::endl;
+        thirtyOneMonth();
+        std::cin.clear();
+        break;
+        case 9:
+        system("clear");
+        std::cout << "    September" << std::endl;
+        thirtyMonth();
+        std::cin.clear();
+        break;
+        case 10:
+        system("clear");
+        std::cout << "     October" << std::endl;
+        thirtyOneMonth();
+        std::cin.clear();
+        break;
+        case 11:
+        system("clear");
+        std::cout << "     November" << std::endl;
+        thirtyMonth();
+        std::cin.clear();
+        break;
+        case 12:
+        system("clear");
+        std::cout << "     December" << std::endl;
+        thirtyOneMonth();
+        std::cin.clear();
+        break;
+        default:
+        std::cin.clear();
+        std::cout << "Please type in a number for your month of choice!" << std::endl;
+        goto monthMistake;
+        break;
+    }
 
     return 0;
 }
